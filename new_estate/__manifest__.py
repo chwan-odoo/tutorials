8dd1fddcd03223e5a1881f8a8140ee1d36d399f5
Chengyan Wang <chwan@odoo.com>
2026-01-06 11:48:47 +0800

    new chapter 3

---
+++ new_estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "New Estate",
+    "version": "1.0",
+    "summary": "Property Selling platform",
+    "sequence": 10,
+    "description": """""",
+    "category": "Sales",
+    "website": "",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "installable": True,
+    "assets": {},
+    "author": "Chengyan(CHWAN)",
+    "license": "LGPL-3",
+}
---
+++ new_estate/models/estate.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+class Estate(models.Model):
+    _name = "estate.property"
+    _description = "This is a new estate module"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        default="north",
+    )
