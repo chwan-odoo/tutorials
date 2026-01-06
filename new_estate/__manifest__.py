b5259bb309d3037b096355de40b5bc3e392fdb96
Chengyan Wang <chwan@odoo.com>
2026-01-06 13:44:17 +0800

    new chapter 5

--- new_estate/__manifest__.py
+++ new_estate/__manifest__.py
@@ -9,6 +9,8 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "demo": [],
     "application": True,
--- new_estate/models/estate.py
+++ new_estate/models/estate.py
@@ -1,4 +1,8 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+def _compute_three_months_from_today():
+    return fields.Date.today() + relativedelta(months=3)
 
 class Estate(models.Model):
     _name = "estate.property"
@@ -7,10 +11,10 @@ class Estate(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(default=_compute_three_months_from_today(), copy=False)
+    expected_price = fields.Float(copy=False)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +28,15 @@ class Estate(models.Model):
             ("west", "West"),
         ],
         default="north",
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
     )
