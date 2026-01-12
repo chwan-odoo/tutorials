698d606ac1fcc706acc764eea2166837ed878a25
Chengyan Wang <chwan@odoo.com>
2026-01-12 15:54:08 +0800

    check point

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,33 @@
+/** @odoo-module **/
+
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
+import { DashboardItem } from "./dashboard_item";
 
-class AwesomeDashboard extends Component {
+export class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { DashboardItem };
+
+    setup() {
+        this.actionService = useService("action");
+    }
+
+    openCustomers() {
+        this.actionService.doAction("base.action_partner_form", {
+            viewType: "kanban"
+        });
+    }
+    openLeads() {
+        this.actionService.doAction({
+            type: 'ir.actions.act_window',
+            name: _t('Leads'),
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
